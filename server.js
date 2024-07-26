68104c05178e44cefb353123824be808ea9e613d
Elton Kaqiu <eltonkaqiu.github@gmail.com>
2024-07-26 10:13:23 +0200

    project setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const dotenv = require(`dotenv`);
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`App listening in port:${PORT}  `));
+
+app.get("/test", (req, res, next) => {
+  res.send(`Hello world!`);
+});

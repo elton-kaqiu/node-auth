20ee469cca3dedb88a1c629f5a9c01d311148d98
Elton Kaqiu <eltonkaqiu.github@gmail.com>
2024-07-26 11:33:16 +0200

    finished role crud operations

--- controllers/role.controller.js
+++ controllers/role.controller.js
@@ -1,28 +1,118 @@
+const { where } = require("sequelize");
 const { Role } = require("../configs/db.config");
 
 // Create and Save a new Role
 const createRole = async (req, res, next) => {
-  // Implementation here
+  const transaction = await Role.sequelize.transaction();
+  try {
+    const { name } = req.body;
+    const existingRole = Role.findOne({ where: { name } });
+    if (existingRole) {
+      await transaction.rollBack();
+      return res
+        .status(400)
+        .json({ message: `Role with name: ${name} already exists!` });
+    }
+    const newRole = await Role.create({ name }, { transaction });
+    await transaction.commit();
+    res.status(201).json(newRole);
+  } catch (error) {
+    await transaction.rollBack();
+    res
+      .status(500)
+      .json({ message: `Something went wrong while creating role!` });
+    next(error);
+  }
 };
 
 // Retrieve all Roles
 const getAllRoles = async (req, res, next) => {
-  // Implementation here
+  try {
+    const roles = await Role.findAll();
+    res.status(200).json(roles);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `Something went wrong while fetching roles!` });
+    next(error);
+  }
 };
 
 // Retrieve a single Role by ID
 const getRoleById = async (req, res, next) => {
-  // Implementation here
+  try {
+    const { id } = req.params;
+    const existingRole = await Role.findOne({ where: { id } });
+    if (!existingRole) {
+      res.status(404).json({ message: `Role with id:${id} doesn't exist!` });
+    }
+    res.status(200).json(existingRole);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `Something went wrong while fetching a role!` });
+    next(error);
+  }
 };
 
 // Update a Role by ID
 const updateRole = async (req, res, next) => {
+  const transaction = await Role.sequelize.transaction();
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+    const existingRole = await Role.findByPk(id);
+    if (!existingRole) {
+      await transaction.rollBack();
+      return res
+        .status(404)
+        .json({ message: `Role with id:${id} does not exist!` });
+    }
+    const roleWithName = await Role.findOne(
+      { where: { name } },
+      { transaction }
+    );
+    if (roleWithName && roleWithName.id !== id) {
+      await transaction.rollBack();
+      return res.status(400).json({ message: "Role name already exists" });
+    }
+
+    existingRole.name = name;
+    await existingRole.save({ transaction });
+    await transaction.commit();
+    res.status(200).json(existingRole);
+  } catch (error) {
+    await transaction.rollBack();
+    res
+      .status(500)
+      .json({ message: `Something went wrong while updating a role!` });
+    next(error);
+  }
+
   // Implementation here
 };
 
 // Delete a Role by ID
 const deleteRole = async (req, res, next) => {
-  // Implementation here
+  const transaction = await Role.sequelize.transaction();
+  try {
+    const { id } = req.params;
+    const existingRole = await Role.findByPk(id);
+    if (!existingRole) {
+      return res.status(404).json({ message: "Role not found" });
+    }
+
+    await existingRole.destroy({ transaction });
+
+    await transaction.commit();
+    res.status(204).json({ message: `Role deleted successfully!` });
+  } catch (error) {
+    await transaction.rollback();
+    res
+      .status(500)
+      .json({ message: "Something went wrong while deleting the role!" });
+    next(error);
+  }
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const app = express();
 const dotenv = require(`dotenv`);
 const db = require("./configs/db.config");
+const path = require("path");
+const userRoutes = require("./routes/user.routes");
+const roleRoutes = require("./routes/role.routes");
 
 dotenv.config();
 
@@ -10,7 +13,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT;
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Use the user routes
+app.use("/api/users", userRoutes);
+
+// Use the role routes
+app.use("/api/roles", roleRoutes);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`App listening in port:${PORT}  `));
 
 app.get("/test", (req, res, next) => {

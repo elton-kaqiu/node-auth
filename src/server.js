57e40b004b283ed77ac722047766ace77365edd8
Elton Kaqiu <eltonkaqiu.github@gmail.com>
2024-07-26 15:48:50 +0200

    server.js to the source folder

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dotenv = require(`dotenv`);
-const db = require("./src/configs/db.config");
-const userRoutes = require("./src/routes/user.routes");
-const roleRoutes = require("./src/routes/role.routes");
+const db = require("./configs/db.config");
+const userRoutes = require("./routes/user.routes");
+const roleRoutes = require("./routes/role.routes");
 
 dotenv.config();
 
